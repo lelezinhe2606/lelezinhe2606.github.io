54cd3b2407fad06ad82b0608f04d6318b06e694f
lelezinhe2606 <leticia.ferreira@estudante.ifro.edu.br>
2024-11-01 14:07:15 -0400

    oi

--- FRONT END II/atividadeonclick.js
+++ FRONT END II/atividadeonclick.js
@@ -1,10 +1,15 @@
-const miniaturas = document.querySelector('.miniatura')
-const imgAmpliada = document.querySelector('.imgAmpliado')
+const miniaturas = document.querySelectorAll('.miniatura')
+const imgAmpliada = document.querySelector('#imgAmpliado')
 
 miniaturas.forEach(miniatura=> {
     miniaturas.addEventListener('click', ()=>{
-        const src = miniatura.src;
-        console.log(src)
-    })
+        miniaturas.forEach(miniatura=>{
+            miniatura.style.opacity = '0.4';
+        })
+        miniatura.computedStyleMap.opacity= '1';
+        imgAmpliada.src = miniatura.src;
+        imgAmpliada.style.display = 'block';
+
+    });
     
-})
+});
