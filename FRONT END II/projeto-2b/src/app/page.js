7a519ededa4ee2f5c495e1cb84f8f943c73851de
lelezinhe2606 <leticia.ferreira@estudante.ifro.edu.br>
2024-11-06 14:31:37 -0400

    oi

--- FRONT END II/projeto-2b/src/app/page.js
+++ FRONT END II/projeto-2b/src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <h1>Meu titulo</h1>
-      <image className = {styles.img} src= '/images/flor.png' alt="imagem flor lilas" width={400} height={400} />
+      <Image className = {styles.img} src= '/Images/flor.png' alt="imagem flor lilas" width={400} height={400} />
     </div>
   );
 }
