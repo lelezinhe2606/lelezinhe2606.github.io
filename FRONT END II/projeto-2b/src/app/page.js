c80b7337c53e99a8fe17448135ac50bbf73d8a6a
lelezinhe2606 <leticia.ferreira@estudante.ifro.edu.br>
2024-11-06 14:25:59 -0400

    oioi

--- FRONT END II/projeto-2b/src/app/page.js
+++ FRONT END II/projeto-2b/src/app/page.js
@@ -1,11 +1,11 @@
-
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <h1>Meu titulo</h1>
-      <input type="text" />
+      <image className = {styles.img} src= '/images/flor.png' alt="imagem flor lilas" width={400} height={400} />
     </div>
   );
 }
