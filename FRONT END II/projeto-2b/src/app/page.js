5c54dbdccb187f3a156defe22aa7d40abdbba112
lelezinhe2606 <leticia.ferreira@estudante.ifro.edu.br>
2024-11-13 13:51:07 -0400

    oioi

--- FRONT END II/projeto-2b/src/app/page.js
+++ FRONT END II/projeto-2b/src/app/page.js
@@ -1,11 +1,1 @@
-import styles from "./page.module.css";
-import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <h1>Meu titulo</h1>
-      <Image className = {styles.img} src= '/Images/flor.png' alt="imagem flor lilas" width={400} height={400} />
-    </div>
-  );
-  }
---
+++ FRONT END II/projeto-2b/src/app/states/page.js
@@ -0,0 +1,28 @@
+'use client'
+import { useState } from "react"
+export default function State() {
+    const [msg, setMsg] = useState('Boa tarde')
+
+    //const alterarMsg = ()=>{
+       // setMsg((prevMsg=>{
+      //      prevMsg = 'Bom dia'
+      //      console.log('msg:'+ msg);
+       //     return prevMsg
+     //   }));
+
+
+     const manipularInput =(evento) => {
+        console.log(evento)
+
+     }
+
+
+    }
+    return(
+        <div>
+            {/*<p>{msg}</p>
+            /*<button onClick={alterarMsg}> alterar</button>*/}
+            <p>Sua Mensagem : {msg}</p>
+            <input type="text" onChange={() => manipularInput()} />
+        </div>
+    )
