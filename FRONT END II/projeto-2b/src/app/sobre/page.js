5523e272188961ad09ad3d99af703c7569dcfdf1
lelezinhe2606 <leticia.ferreira@estudante.ifro.edu.br>
2024-11-24 21:06:13 -0400

    Deus

--- FRONT END II/projeto-2b/src/app/sobre/page.js
+++ FRONT END II/projeto-2b/src/app/sobre/page.js
@@ -1,10 +1,10 @@
 import styles from './sobre.module.css'
-const Sobre = () => {
-    return(
-        <main>
-            <h1 className={styles.h1}>Pagina Sobre</h1>
-        </main>
-    )
-}
 
-export default Sobre;
+export default function Sobre() {
+  return (
+    <main>
+      <h1 className={styles.h1}>olivia Rodrigo</h1>
+    
+    </main>
+  );
+}
