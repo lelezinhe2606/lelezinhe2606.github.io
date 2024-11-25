ad8b2b9c989fe004d69d0b5266eb4f823ac172c4
lelezinhe2606 <leticia.ferreira@estudante.ifro.edu.br>
2024-11-24 20:11:05 -0400

    nextt

--- FRONT END II/projeto-2b/next.config.mjs
+++ FRONT END II/projeto-2b/next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true, // Habilita o modo estrito do React
-    experimental: {
-      appDir: true, // Garante que a App Router do Next.js está habilitada
-    },
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
